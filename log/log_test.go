2e4b948f27c2c3d4834befe08314a5056a70021c
chenqiang <qiang.chen@igg.com>
2021-10-12 14:46:39 +0800

    Add log test

---
+++ log/log_test.go
@@ -0,0 +1,13 @@
+// Copyright © 2020-present ichenq@outlook.com All rights reserved.
+// Distributed under the terms and conditions of the BSD License.
+// See accompanying files LICENSE.
+
+package log
+
+import (
+	"testing"
+)
+
+func TestSetup(t *testing.T) {
+	Setup(NewConfig("debug"))
+}
