76a5d8d04d87ed91b2021cf59c1e39914c85101f
chenqiang <qiang.chen@igg.com>
2021-10-22 14:49:59 +0800

    add version

---
+++ version.go
@@ -0,0 +1,7 @@
+// Copyright © 2020-present ichenq@outlook.com All rights reserved.
+// Distributed under the terms and conditions of the BSD License.
+// See accompanying files LICENSE.
+
+package fatchoy
+
+const VERSION = "1.1.0"
