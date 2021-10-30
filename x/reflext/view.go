b502a798b73999fcac6f868ad0c51617449ca41c
chenqiang <qiang.chen@igg.com>
2021-10-30 14:23:40 +0800

    update reflect package

---
+++ x/reflext/view.go
@@ -0,0 +1,5 @@
+// Copyright © 2021-present ichenq@outlook.com All rights reserved.
+// Distributed under the terms and conditions of the BSD License.
+// See accompanying files LICENSE.
+
+package reflext
