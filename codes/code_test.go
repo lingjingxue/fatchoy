7c8be817d3fcd062d38c7a0ccd1eadd88ead932b
chenqiang <qiang.chen@igg.com>
2021-11-09 13:52:43 +0800

    add test

---
+++ codes/code_test.go
@@ -0,0 +1,28 @@
+// Copyright © 2021-present ichenq@outlook.com All rights reserved.
+// Distributed under the terms and conditions of the BSD License.
+// See accompanying files LICENSE.
+
+package codes
+
+import (
+	"testing"
+)
+
+func TestCode(t *testing.T) {
+	tests := []struct {
+		code Code
+		name string
+	}{
+		{Unknown, "UNKNOWN"},
+		{ServiceMaintenance, "SERVICE_MAINTENANCE"},
+		{RequestTimeout, "REQUEST_TIMEOUT"},
+	}
+
+	for _, tc := range tests {
+		var name = GetCodeName(tc.code)
+		var val = GetCodeValue(tc.name)
+		if name != tc.name || val != tc.code {
+			t.Fatalf("%v", tc.code)
+		}
+	}
+}
