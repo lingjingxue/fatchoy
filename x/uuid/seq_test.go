e02d55c1b6279e2f0d1fecad81404c4fef580446
chenqiang <qiang.chen@igg.com>
2021-11-09 16:22:49 +0800

    fix test

--- x/uuid/seq_test.go
+++ x/uuid/seq_test.go
@@ -5,13 +5,51 @@
 package uuid
 
 import (
+	"context"
+	"fmt"
+	"log"
 	"testing"
 	"time"
+
+	"go.etcd.io/etcd/clientv3"
 )
 
-func TestSeqIDEtcdSimple(t *testing.T) {
-	var store = createEtcdStore("/uuid/ctr101")
-	var seq = NewSeqID(store, 2000)
+var (
+	etcdAddr  = "localhost:2379"
+	redisAddr = "localhost:6379"
+	mongoUri  = "mongodb://admin:cuKpVrfZzUvg@127.0.0.1:27017/?connect=direct"
+	mysqlDSN  = "root:LGrk4IaS0Wflxw@tcp(localhost:3306)/testdb"
+)
+
+func createCounterStorage(storeTye string, label string) Storage {
+	ctx, _ := context.WithTimeout(context.Background(), time.Second*30)
+	switch storeTye {
+	case "etcd":
+		client, err := clientv3.New(clientv3.Config{
+			Endpoints:   []string{etcdAddr},
+			DialTimeout: time.Second * OpTimeout,
+		})
+		if err != nil {
+			log.Panicf("cannot connect etcd: %v", err)
+		}
+		return NewEtcdStore(ctx, client, label)
+
+	case "mongo":
+		var db = "testdb"
+		return NewMongoDBStore(ctx, mongoUri, db, label, DefaultSeqStep)
+	case "mysql":
+		return NewMySQLStore(ctx, mysqlDSN, "uuid", label, 2000)
+	case "redis":
+		return NewRedisStore(ctx, redisAddr, label)
+	default:
+		panic(fmt.Sprintf("invalid storage type %s", storeTye))
+	}
+	return nil
+}
+
+func runSeqIDTestSimple(t *testing.T, storeTye, label string) {
+	var store = createCounterStorage(storeTye, label)
+	var seq = NewSeqID(store, DefaultSeqStep)
 	if err := seq.Init(); err != nil {
 		t.Fatalf("Init: %v", err)
 	}
@@ -27,16 +65,17 @@ func TestSeqIDEtcdSimple(t *testing.T) {
 	}
 	var elapsed = time.Now().Sub(start).Seconds()
 	t.Logf("etcd QPS %.2f/s", float64(tetLoad)/elapsed)
-	// Output:
-	//   etcd QPS: 3236293.09/s
 }
 
 // N个并发worker，共享一个生成器, 测试生成id的一致性
-func TestSeqIDEtcdConcurrent(t *testing.T) {
-	var gcnt = 10
-	var eachMax = 100000
-	var store = createEtcdStore("/uuid/ctr103")
-	var seq = NewSeqID(store, 2000)
+func runSeqIDTestConcurrent(t *testing.T, storeTye, label string) {
+	var gcnt = 20
+	var eachMax = 500000
+	var store = createCounterStorage(storeTye, label)
+	var seq = NewSeqID(store, DefaultSeqStep)
+	if err := seq.Init(); err != nil {
+		t.Fatalf("Init: %v", err)
+	}
 	var workerCtx = NewWorkerContext(eachMax, func() IDGenerator { return seq })
 	for i := 0; i < gcnt; i++ {
 		workerCtx.Go(t, i)
@@ -47,16 +86,22 @@ func TestSeqIDEtcdConcurrent(t *testing.T) {
 	if !t.Failed() {
 		t.Logf("QPS %.2f/s", float64(gcnt*eachMax)/elapsed)
 	}
-	// Output:
-	//    QPS 2096858.84/s
 }
 
 // N个并发worker，每个worker单独生成器, 测试生成id的一致性
-func TestSeqIDEtcdDistributed(t *testing.T) {
-	var gcnt = 10
-	var eachMax = 100000
-	var store = createEtcdStore("/uuid/ctr103")
-	var workerCtx = NewWorkerContext(eachMax, func() IDGenerator { return NewSeqID(store, 2000) })
+func runSeqIDTestDistributed(t *testing.T, storeTye, label string) {
+	var gcnt = 20
+	var eachMax = 500000
+	var generator = func() IDGenerator {
+		var store = createCounterStorage(storeTye, label)
+		var seq = NewSeqID(store, DefaultSeqStep)
+		if err := seq.Init(); err != nil {
+			t.Fatalf("Init: %v", err)
+		}
+		return seq
+	}
+
+	var workerCtx = NewWorkerContext(eachMax, generator)
 	for i := 0; i < gcnt; i++ {
 		workerCtx.Go(t, i)
 	}
@@ -66,6 +111,76 @@ func TestSeqIDEtcdDistributed(t *testing.T) {
 	if !t.Failed() {
 		t.Logf("QPS %.2f/s", float64(gcnt*eachMax)/elapsed)
 	}
+}
+
+func TestSeqIDEtcdSimple(t *testing.T) {
+	runSeqIDTestSimple(t,"etcd", "/uuid/counter1")
 	// Output:
-	//  QPS 2174803.37/s
+	//  QPS 3209546.48/s
+}
+
+func TestSeqIDEtcdConcurrent(t *testing.T) {
+	runSeqIDTestConcurrent(t,"etcd", "/uuid/counter2")
+	// Output:
+	//  QPS 1932994.78/s
+}
+
+func TestSeqIDEtcdDistributed(t *testing.T) {
+	runSeqIDTestDistributed(t,"etcd", "/uuid/counter3")
+	// Output:
+	//  QPS 2105700.17/s
+}
+
+func TestSeqIDRedisSimple(t *testing.T) {
+	runSeqIDTestSimple(t,"redis", "/uuid/counter1")
+	// Output:
+	//  QPS 4792038.70/s
+}
+
+func TestSeqIDRedisConcurrent(t *testing.T) {
+	runSeqIDTestConcurrent(t,"redis", "/uuid/counter2")
+	// Output:
+	//  QPS 2222480.03/s
+}
+
+func TestSeqIDRedisDistributed(t *testing.T) {
+	runSeqIDTestDistributed(t,"redis", "/uuid/counter3")
+	// Output:
+	//  QPS 2462537.90/s
+}
+
+func TestSeqIDMongoSimple(t *testing.T) {
+	runSeqIDTestSimple(t,"mongo", "uuid_counter1")
+	// Output:
+	//  QPS 3325821.41/s
+}
+
+func TestSeqIDMongoConcurrent(t *testing.T) {
+	runSeqIDTestConcurrent(t,"mongo", "uuid_counter2")
+	// Output:
+	//  QPS 1880948.45/s
+}
+
+func TestSeqIDMongoDistributed(t *testing.T) {
+	runSeqIDTestDistributed(t,"mongo", "uuid_counter3")
+	// Output:
+	//  QPS 2380477.59/s
+}
+
+func TestSeqIDMySQLSimple(t *testing.T) {
+	runSeqIDTestSimple(t,"mysql", "uuid_counter1")
+	// Output:
+	//  QPS 1403723.91/s
+}
+
+func TestSeqIDMySQLConcurrent(t *testing.T) {
+	runSeqIDTestConcurrent(t,"mysql", "uuid_counter2")
+	// Output:
+	//  QPS 1084712.19/s
+}
+
+func TestSeqIDMySQLDistributed(t *testing.T) {
+	runSeqIDTestDistributed(t,"mysql", "uuid_counter3")
+	// Output:
+	//  QPS 1966049.74/s
 }
