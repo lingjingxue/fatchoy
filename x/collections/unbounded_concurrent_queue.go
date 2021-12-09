cb73b0ed29423d816b42d0819f2af5395e9c2ffe
simon <simon@qchen.fun>
2021-12-09 11:18:58 +0800

    add UnboundedConcurrentQueue

---
+++ x/collections/unbounded_concurrent_queue.go
@@ -0,0 +1,38 @@
+// Copyright © 2020 simon@qchen.fun All rights reserved.
+// Distributed under the terms and conditions of the BSD License.
+// See accompanying files LICENSE.
+
+package collections
+
+import (
+	"sync"
+)
+
+type UnboundedConcurrentQueue struct {
+	queue UnboundedQueue
+	guard sync.RWMutex
+}
+
+func NewUnboundedConcurrentQueue() *UnboundedConcurrentQueue {
+	return &UnboundedConcurrentQueue{}
+}
+
+func (q *UnboundedConcurrentQueue) Len() int {
+	q.guard.RLock()
+	var n = q.queue.Len()
+	q.guard.RUnlock()
+	return n
+}
+
+func (q *UnboundedConcurrentQueue) Enqueue(item interface{}) {
+	q.guard.Lock()
+	q.queue.Push(item)
+	q.guard.Unlock()
+}
+
+func (q *UnboundedConcurrentQueue) Dequeue() (v interface{}, ok bool) {
+	q.guard.Lock()
+	v, ok = q.queue.Pop()
+	q.guard.Unlock()
+	return
+}
---
+++ x/collections/unbounded_concurrent_queue_test.go
@@ -0,0 +1,16 @@
+// Copyright © 2020 simon@qchen.fun All rights reserved.
+// Distributed under the terms and conditions of the BSD License.
+// See accompanying files LICENSE.
+
+package collections
+
+import (
+	"testing"
+)
+
+func TestUnboundedConcurrentQueue_Enqueue(t *testing.T) {
+	var q = NewUnboundedConcurrentQueue()
+	for i := 0; i < 10; i++ {
+		q.Enqueue(i)
+	}
+}
